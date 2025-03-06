863cf5c37b7bda3e808d343d40a6ba6107fd30af
sabbirafmkhan <sabbirafmkhan@gmail.com>
2025-03-06 22:01:25 +0600

    setup

---
+++ android/app/src/main/kotlin/com/youtube_lite/youtube_lite/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.youtube_lite.youtube_lite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/webpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WebPage extends StatefulWidget {
+  const WebPage({super.key});
+
+  @override
+  State<WebPage> createState() => _WebPageState();
+}
+
+class _WebPageState extends State<WebPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
